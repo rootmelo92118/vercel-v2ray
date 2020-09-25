95361b176eddb61646b2ae91b757a8e2c8c1c4d5
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2020-09-25 14:58:06 +0800

    Update launch.py

--- launch.py
+++ launch.py
@@ -4,7 +4,7 @@ core = urllib.request.urlopen("https://github.com/v2fly/v2ray-core/releases/late
 with open("core.zip", "wb") as binaryCode:
   binaryCode.write(core.read())
   binaryCode.close()
-setupFileContent = "unzip core.zip v2ray v2ctl geoip.dat geosite.dat\nchmod +x *\nrm core.zip\nnohup ./v2ray -config=" + URL_Param + "&"
+setupFileContent = "unzip core.zip v2ray v2ctl geoip.dat geosite.dat\nchmod +x *\nrm core.zip\nnohup ./v2ray -config=" + URL_Param
 setupFile = open("setup.sh", "a")
 setupFile.write(setupFileContent)
 setupFile.close()
