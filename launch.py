57f1ef99b9c9b18ef5004294b8fff253de13f259
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2020-09-25 12:12:40 +0800

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,2 +1,7 @@
-import sys
+import sys, urllib, os
 URL_Parame = sys.argv[1]
+core = urllib.request.urlopen("https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip")
+with open("core.zip", "wb") as binaryCode:
+  binaryCode.write(core.read())
+  binaryCode.close()
+setupFile = 
